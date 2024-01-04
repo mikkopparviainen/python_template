f57f31aa58e5377f81f715931ad4b724834291e3
Mikko Parviainen <mikkopa@gmail.com>
2024-01-04 22:42:00 +0200

    Initial commit

---
+++ python_template.py
@@ -0,0 +1,42 @@
+import torch
+import sys
+
+import seaborn as sn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+# Hello world function
+fs = 250
+f = 5
+T = 2
+
+
+def pp(f, T, fs):
+    """
+
+    :param f: the frequency of a signal
+    :param T: the length of the signal in seconds
+    :param fs: the samplling rate
+    :return: s: signal, t: time axis in seconds
+    """
+
+    t = np.linspace(0,T,fs)
+    s = np.sin(2*np.pi*f*t)
+    return s, t
+
+
+
+if __name__ == "__main__":
+    # print version information
+    print(f"Python version: {sys.version}")
+    print(f"Pytorch version: {torch.__version__}")
+    print(f"Numpy version: {np.__version__}")
+    print(f"Seaborn version: {sn.__version__}")
+    print(f"Pandas version: {pd.__version__}")
+    print(f"Matplotlib version: {matplotlib.__version__}")
+    s, t = pp(f,T,fs)
+    plt.plot(t, s, 'o-')
+    plt.show()
