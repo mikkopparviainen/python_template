11e5948c56cab290e02895189ffeb00e48ad130b
Mikko Parviainen <mikkopa@gmail.com>
2024-01-06 21:26:10 +0200

    A new branch called dev_python_tempalate with automatic module discovery

--- python_template.py
+++ python_template.py
@@ -2,8 +2,11 @@ import torch
 import sys
 
 import seaborn as sn
+import seaborn
 import pandas as pd
+import pandas
 import numpy as np
+import numpy
 import matplotlib.pyplot as plt
 import matplotlib
 
@@ -27,17 +30,36 @@ def pp(f, T, fs):
     s = np.sin(2*np.pi*f*t)
     return s, t
 
+import types
+# Find out what modules are imported
+def imports():
+    for name, val in globals().items():
+        if isinstance(val, types.ModuleType):
+            yield val.__name__
 
 
 if __name__ == "__main__":
     # print version information of python and imported packages.
     print(f"Python version: {sys.version}")
-    print(f"Pytorch version: {torch.__version__}")
-    print(f"Numpy version: {np.__version__}")
-    print(f"Seaborn version: {sn.__version__}")
-    print(f"Pandas version: {pd.__version__}")
-    print(f"Matplotlib version: {matplotlib.__version__}")
+    #print(f"Pytorch version: {torch.__version__}")
+    #print(f"Numpy version: {np.__version__}")
+    #print(f"Seaborn version: {sn.__version__}")
+    #print(f"Pandas version: {pd.__version__}")
+    #print(f"Matplotlib version: {matplotlib.__version__}")
     # Generate signal and plot it
     s, t = pp(f,T,fs)
     plt.plot(t, s, 'o-')
-    plt.show()
+    #plt.show()
+
+
+    # A new version to find module versions automatically. TODO: what about modules that do not have version in __version__
+    modulenames = list(imports())
+    modulenames = list(set(modulenames))
+    for m in modulenames:
+        try:
+            version_id = eval(m).__version__
+            print(f"Module {m} version: {version_id}")
+        except (AttributeError, NameError):
+            #print(f"No version info for module {m} in .__version__")
+            continue
+    pass
